1dcd04c6fe9d78991cb07687188df8de810555b0
Jeff Nabors <bulletr0k@gmail.com>
2018-01-13 01:11:47 -0500

    Finished twitter, finished Spotify, finished OMDB API call

--- liri.js
+++ liri.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 var twitter = require("twitter");
-
+var Spotify = require("node-spotify-api");
 const keys = require("./keys.js");
 //Twitter API Keys
 var client = new twitter(keys.twitter);
 // Spotify API Keys
-var spotify = keys.spotify;
+var spotify = new Spotify(keys.spotify);
 
 //------------------------
 
@@ -13,11 +13,70 @@ var nodeArgs = process.argv;
 var params = {
     screen_name: 'T1BasicMountain'
 }
-client.get('statuses/user_timeline', params, function (error, tweets, response) {
-    if (!error) {
-        for (var i = 19; i > 0; i--) {
-            console.log(tweets[i].text)
-        };
+//This function will pull the most recent tweets for my twitter profile (this profile has only 20 tweets, all forming the poem Invictus by William E. Henley, at the time of writing this at least)
+function recallTweets() {
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            tweets.reverse().forEach(element => {
+                console.log(`
+            ------
+            ${element.text}`
+                    //${element.created_at} was omitted because my 20 tweets are park fo a poem, and that makes the formatting look ugly.
+                );
+            });
+            // console.log(tweets[0]);
 
-    }
-})
+        }
+    })
+};
+
+var songTitle = "Time Consumer";
+
+//if songTitle is blank, replace it with 'The Sign'
+function playSong(title) {
+    spotify.search({
+        type: 'track',
+        query: title,
+        limit: 1
+    }, function (err, data) {
+        if (!err) {
+            var songObj = data.tracks.items[0];
+            var artistName = songObj.album.artists[0].name;
+            console.log(`
+            Artist: ${artistName}
+            Song title: ${title}
+            Album Name: ${songObj.album.name}
+            Preview URL: ${songObj.preview_url}`);
+            // console.log(songObj);
+        } else {
+            console.log(`Error: ${err}`);
+        }
+    })
+}
+// playSong(songTitle);
+
+//OMDB API call
+
+var request = require("request");
+var movieName = "";
+//Parse movieName to add +'s
+function movieQuery() {
+    request(`http://www.omdbapi.com/?t=${movieName}&apikey=trilogy`, function (err, response, body) {
+        if (!err && response.statusCode === 200) {
+            var movieObj = JSON.parse(body);
+            // console.log(movieObj);
+            console.log(`
+                Movie Title: ${movieObj.Title}
+                Year Released: ${movieObj.Year}
+                IMDB Rating: ${movieObj.Ratings[0].Value}
+                Rotten Tomtoes Rating: ${movieObj.Ratings[1].Value}
+                Produced in: ${movieObj.Country}
+                Language(s): ${movieObj.Language}
+                Plot: ${movieObj.Plot}
+                Actors: ${movieObj.Actors}
+                 `);
+        }
+    })
+};
+
+var fs = require("fs");
